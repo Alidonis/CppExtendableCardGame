b17fc380141f1982273fe7916da8a6dd859ea1ac
Alidonis <64419217+Alidonis@users.noreply.github.com>
2023-03-11 21:43:15 +0100

    Regular backup of code

--- src/CBaseGame.cpp
+++ src/CBaseGame.cpp
@@ -6,6 +6,15 @@ CBaseGame::CBaseGame()
 }
 
 void CBaseGame::addPlayer(int id)
-{
-
-} 
+{ //TODO : validate id
+	bool isValidId = true; //gonna make it in a copy to observe the changes
+	if (id < 0) { return; }
+	for (int i = 0; i < players.size(); i++)
+	{
+		if (players[i]->getId() == id)
+		{
+			return;
+		}
+	}
+	players.push_back(std::make_unique<CPlayer>(id));
+}
