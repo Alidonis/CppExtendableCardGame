c9f13d73630636e911ef7b0d184b23c729c9ee20
atsun16 <81156312+atsun16@users.noreply.github.com>
2023-05-31 11:36:05 +0200

    more gitignore plus moving some code around

--- src/CWindowDebug.cpp
+++ src/CWindowDebug.cpp
@@ -4,7 +4,25 @@
 #include "windowlinker.h"
 #include "BaseWindow.h"
 
+#include "CBaseCard.h"
+#include "cardlinker.h"
+
 #include "CWindowDebug.h"
+
+std::vector<std::string> Debug_Util_RegisteredCardNames(bool print = true, bool instanciate = false)
+{
+    std::vector<std::string> cardNames;
+    if (print) { std::cout << "Print start" << std::endl; }
+    for (size_t i = 0; i < CardLinker_Pairs.size(); i++)
+    {
+        cardNames.push_back(std::string(CardLinker_Pairs[i]->classname));
+        if (print) { std::cout << std::string(CardLinker_Pairs[i]->classname) << std::endl; }
+        if (instanciate) { CBaseCard* instantiationTest = CardLinker_Pairs[i]->factory(); std::cout << "Instantiated card name : " << instantiationTest->getName() << std::endl; delete instantiationTest; }
+    }
+
+    if (print) { std::cout << "Print end" << std::endl; }
+    return cardNames;
+}
 
 void CWindowDebug::render()
 {
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -16,21 +16,6 @@
 
 #include "renderer.h"
 
-std::vector<std::string> Debug_Util_RegisteredCardNames(bool print = true, bool instanciate = false)
-{
-    std::vector<std::string> cardNames;
-    if (print) { std::cout << "Print start" << std::endl; }
-    for (size_t i = 0; i < CardLinker_Pairs.size(); i++)
-    {
-        cardNames.push_back(std::string(CardLinker_Pairs[i]->classname));
-        if (print) { std::cout << std::string(CardLinker_Pairs[i]->classname) << std::endl; }
-        if (instanciate) { CBaseCard* instantiationTest = CardLinker_Pairs[i]->factory(); std::cout << "Instantiated card name : " << instantiationTest->getName() << std::endl; delete instantiationTest; }
-    }
-
-    if (print) { std::cout << "Print end" << std::endl; }
-    return cardNames;
-}
-
 renderer::renderer(bool runImmediately = true) {
     // Step 1. Init SDL and create window
     SDL_Init(SDL_INIT_VIDEO);
